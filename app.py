e98b418950aef44fff6d1609e0ef28f7c21caa78
matimove <timo.v.makela@helsinki.fi>
2026-06-28 21:47:53 +0300

    Added page count limit checks to edit_book

--- app.py
+++ app.py
@@ -771,6 +771,12 @@ def edit_book(book_id):
 
                 if page_count <= 0:
                     raise ValueError
+                
+              
+                if page_count > 10000:
+                    raise ValueError
+                
+                
 
             except ValueError:
                 flash('Invalid page count!', 'error')
